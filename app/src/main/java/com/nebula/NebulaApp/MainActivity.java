57cdd248c6069451bd87a94fefd9cca69a7031b6
AlphaVerse <vortexcode06@gmail.com>
2024-02-17 19:43:36 +0530

    initial

---
+++ app/src/main/java/com/nebula/NebulaApp/MainActivity.java
@@ -0,0 +1,131 @@
+package com.nebula.NebulaApp;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        //make fully Android Transparent Status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+//        MaterialCalendarView calendarView = findViewById(R.id.calendarView);
+//        calendarView.setPadding(10,10,10,10);
+//
+//        // Create a list of dates to highlight
+//        List<CalendarDay> datesToHighlight = new ArrayList<>();
+//        Calendar calendar = Calendar.getInstance();
+//        // Set the start date
+//        calendar.set(2024, Calendar.JANUARY, 1);
+//        // Loop through the days and add them to the list
+//        for (int i = 0; i < 21; i++) {
+////            LocalDate localDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+//            datesToHighlight.add(CalendarDay.from(calendar));
+//
+//            // Increment the date by one day
+//            calendar.add(Calendar.DATE, 1);
+//        }
+//
+//        // Create a new decorator instance
+//        EventDecorator decorator = new EventDecorator(Color.GREEN, datesToHighlight);
+//
+//        // Add decorator
+//        calendarView.addDecorator(decorator);
+//        MaterialDatePicker.Builder.datePicker().setSelection(MaterialDatePicker.todayInUtcMilliseconds());
+
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setItemActiveIndicatorColor(getColorStateList(R.color.menuItemActiveIndicator));
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.navHome){
+                    getFragment(new HomeFragment(), false);
+                } else if (itemId == R.id.navAttendance) {
+                    getFragment(new AttendanceFragment(), false);
+                } else if (itemId == R.id.navLeave) {
+                    getFragment(new LeaveFragment(), false);
+                }else{ // navProfile
+                    getFragment(new ProfileFragment(), false);
+                }
+                return true;
+            }
+        });
+        getFragment(new HomeFragment(), true);
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    private void getFragment(Fragment fragment, boolean isInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(isInitialized){
+            fragmentTransaction.add(R.id.frameLayout, new HomeFragment());
+        }else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
+    }
+}
